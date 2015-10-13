e6b148d87055ab8fde2422e355ae88d0ef6d78b6
supremefist <sakkie99@gmail.com>
2015-10-14 00:14:12 +0200

    Minor.

--- python/roster_tracker/widgets/export_data.py
+++ python/roster_tracker/widgets/export_data.py
@@ -11,9 +11,21 @@ class ExportDataWidget(QtGui.QWidget):
         
         self._start_calendar = QtGui.QCalendarWidget(self)
         self._start_calendar.setGridVisible(True)
+        self._start_calendar.clicked[QtCore.QDate].connect(self.start_selected)
         self._layout.addWidget(self._start_calendar, 0, 0)
+        
         self._end_calendar = QtGui.QCalendarWidget(self)
         self._end_calendar.setGridVisible(True)
+        self._end_calendar.clicked[QtCore.QDate].connect(self.end_selected)
         self._layout.addWidget(self._end_calendar, 0, 1)
         
         self.setLayout(self._layout)
+        
+    def start_selected(self, date):
+        self._end_calendar.setDateRange(date,
+                                        self._end_calendar.maximumDate());
+    
+    def end_selected(self, date):
+        self._start_calendar.setDateRange(self._start_calendar.minimumDate(),
+                                          date);
+        
